affcefd5729cea5d6ecdb8459e2c21d196a64696
Shahan <shahan.ahmed@.apify.ai>
2022-08-11 15:07:23 +0500

    admin and healthcheck APIs testing covered

--- src/tests/api/restful/admin.test.ts
+++ src/tests/api/restful/admin.test.ts
@@ -24,32 +24,32 @@ describe('RESTful - Admin Authentication APIs', function () {
 
 	it('admin loggedIn', async () => {
 		const { body: loginBody } = await adminHelper.login();
-    const token = loginBody.token;
+		const token = loginBody.token;
 
-    const res = await adminHelper.loggedIn(token);
+		const res = await adminHelper.loggedIn(token);
 		expect(res.error).to.be.false;
 		expect(res.status).to.be.equal(200);
 		expect(res.body).to.be.an('object');
 		expect(res.body).not.to.have.property('password');
 	});
 
-  it('admin changePassword', async () => {
+	it('admin changePassword', async () => {
 		const { body: loginBody } = await adminHelper.login();
-    const token = loginBody.token;
+		const token = loginBody.token;
 
-    let res = await adminHelper.changePassword('123abc456', 'shahan', token);
+		let res = await adminHelper.changePassword('123abc456', 'shahan', token);
 		expect(res.error).not.to.be.false;
-    expect(res.status).to.be.equal(409);
-    expect(res.text).to.be.a('string');
+		expect(res.status).to.be.equal(409);
+		expect(res.text).to.be.a('string');
 
-    res = await adminHelper.changePassword('123Abc456', '123aBc456', token);
-    expect(res.error).to.be.false;
-    expect(res.status).to.be.equal(200);
-    expect(res.text).to.be.a.string('password changed successfully')
+		res = await adminHelper.changePassword('123Abc456', '123aBc456', token);
+		expect(res.error).to.be.false;
+		expect(res.status).to.be.equal(200);
+		expect(res.text).to.be.a.string('password changed successfully');
 
-    res = await adminHelper.changePassword('123aBc456', '123Abc456', token);
-    expect(res.error).to.be.false;
-    expect(res.status).to.be.equal(200);
-    expect(res.text).to.be.a.string('password changed successfully')
-  })
+		res = await adminHelper.changePassword('123aBc456', '123Abc456', token);
+		expect(res.error).to.be.false;
+		expect(res.status).to.be.equal(200);
+		expect(res.text).to.be.a.string('password changed successfully');
+	});
 });
