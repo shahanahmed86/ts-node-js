fb89add0819e4ebb6c24895dfaf5ae30f090f866
Shahan <shahan.ahmed@.apify.ai>
2022-08-28 16:52:35 +0500

    fixes

--- src/library/morgan.library.ts
+++ src/library/morgan.library.ts
@@ -4,14 +4,14 @@ import morgan from 'morgan';
 import { IRequest } from '../types/extends.types';
 import { IN_PROD } from '../config/app.config';
 import { MORGAN_TOKENS } from '../utils/constants.utils';
+import { Request, Response } from 'express';
 
 morgan.token('host', os.hostname);
 morgan.token('error', (req: IRequest) => (req.error ? req.error.message : ''));
-const errorStream = IN_PROD ? fs.createWriteStream('./logs/error.log', { flags: 'a' }) : undefined;
 
-const logger = morgan(MORGAN_TOKENS, {
-	skip: (_, res) => (IN_PROD ? res.statusCode < 400 : false),
-	stream: errorStream,
-});
+const skip = (_req: Request, res: Response) => (IN_PROD ? res.statusCode < 400 : false);
+const stream = IN_PROD ? fs.createWriteStream('./logs/error.log', { flags: 'a' }) : undefined;
+
+const logger = morgan(MORGAN_TOKENS, { skip, stream });
 
 export default logger;
