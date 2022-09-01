73140567c2a02a511b4eb70f40f81ed48f337bd6
Shahan <shahan.ahmed@.apify.ai>
2022-09-01 12:49:41 +0500

    JWT expiry added to environment variables

--- src/utils/constants.utils.ts
+++ src/utils/constants.utils.ts
@@ -18,6 +18,6 @@ export const GENDER_OPTIONS: string[] = ['MALE', 'FEMALE', 'PREFER_NOT_TO_SAY'];
 
 export const LOGIN_TYPES: string[] = ['LOCAL', 'FACEBOOK', 'GOOGLE'];
 
-export const EXPIRES_IN_MILLISECONDS: number = JWT_EXPIRES; // one minute
+export const EXPIRES_IN_MILLISECONDS: number = JWT_EXPIRES;
 
-export const EXPIRES_IN_SECONDS: number = EXPIRES_IN_MILLISECONDS / 1000; // one minute
+export const EXPIRES_IN_SECONDS: number = EXPIRES_IN_MILLISECONDS / 1000;
